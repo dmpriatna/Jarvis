dc398f1ec95a2cfed7bb5d076a82ba353dc2d0ee
dmpriatna <dedemaulanapriatna@gmail.com>
2022-06-14 21:11:49 +0700

    improve style and script

--- views/PROFILE/js/main.js
+++ views/PROFILE/js/main.js
@@ -132,6 +132,42 @@
 
         portfolioIsotope.isotope({filter: $(this).data('filter')});
     });
+
+    // Service carousel
+    $(".service-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        margin: 30,
+        dots: false,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+            '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+        ],
+        responsive: {
+            0:{
+                items:1
+            },
+            576:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:2
+            }
+        }
+    });
+    
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        items: 1,
+        loop: true,
+    });
     
 })(jQuery);
 
