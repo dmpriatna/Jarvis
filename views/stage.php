4c2ed78e6bb9cd76736b3df5daa38dbaaedb1996
RafiAhnaf <110021906+RafiAhnaf@users.noreply.github.com>
2022-07-29 16:28:20 +0700

    Update

--- views/stage.php
+++ views/stage.php
@@ -61,7 +61,7 @@
     <div class="wow fadeInUp" data-wow-delay="0.1s">
         <div class="container-fluid">
             <div class="row">
-                <div class="col-lg-5 col-md-12 brr-100 bc-1" style="position: relative; margin-bottom: 50px; padding: 20px 50px;">
+                <div class="col-lg-5 col-md-12 brr-100 bc-1" style="position: relative; margin-bottom: 50px; padding: 0 50px;">
                     <div class="v-center">
                         <h3 class="c-3 f-32-b">Apakah Anda Sulit Mengelola UMKM Binaan Dalam Hal Ini?</h3>
                         <div class="line-3 c-3 bc-3" style="margin: 20px 0;"></div>
@@ -120,7 +120,7 @@
                 <span class="f-21-b c-1" style="font-size: 20px;">Event tambahan pembawa potensi kolaborasi.</span>
                 <div class="line-1 c-3 bc-3" style="margin: 20px 0;"></div>
                 <img src="picture/event.png" alt="event" style="border-top-right-radius: 40px; width: 100%;">
-                <span class="c-1" style="padding-top: 20px;">Menambahkan value lebih jadi ciri khas kami dalam mendampingi, anda bisa sekaligus berkolaborasi bersama partner kami setelahnya.</span>
+                <span class="c-1">Menambahkan value lebih jadi ciri khas kami dalam mendampingi, anda bisa sekaligus berkolaborasi bersama partner kami setelahnya.</span>
             </div>
         </div>
         <div class="row py-5">
