91164cd77fd155263799de5623151123030f25bd
RafiAhnaf <110021906+RafiAhnaf@users.noreply.github.com>
2022-07-31 17:47:06 +0700

    Update

--- views/stage.php
+++ views/stage.php
@@ -142,7 +142,7 @@
                         <a class="nav-link active" href="/#" style="font-size: 20px;">Bisnis</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="/#" style="font-size: 20px; color: #0B473B;">Desain</a>
+                        <a class="nav-link" href="/#" style="font-size: 20px; color: var(--color-theme-2);">Desain</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="/#" style="font-size: 20px;">Marketing</a>
