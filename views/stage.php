eaabaa31a065f34a276d9009bcc5cc71f6c7b29a
RafiAhnaf <110021906+RafiAhnaf@users.noreply.github.com>
2022-07-31 18:13:40 +0700

    Update

--- views/stage.php
+++ views/stage.php
@@ -85,7 +85,7 @@
                     </div>
                 </div>
                 <div class="col-lg-7 col-md-12 brl-100" style="margin-left: -50px;">
-                    <img src="picture/section-2.png" alt="section-2" style="border-bottom-left-radius: 100px; height: 100%; width: 760px; left: auto;">
+                    <img src="picture/section-2.png" alt="section-2" style="border-bottom-left-radius: 100px; position: relative; height: 100%; width: 760px;">
                 </div>
             </div>
         </div>
