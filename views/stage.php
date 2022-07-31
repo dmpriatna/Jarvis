1c92c9244f98d29ef8bf301ce403c9cb6d896ab3
RafiAhnaf <110021906+RafiAhnaf@users.noreply.github.com>
2022-07-31 17:19:05 +0700

    Update

--- views/stage.php
+++ views/stage.php
@@ -131,8 +131,8 @@
         <div class="row py-5">
             <div class="col-md-6">
                 <h1 class="v-line-6" style="padding-left: 25px;"><b>Hasil Tidak Akan</b> Mengkhianati Usaha</h1>
-                <div class="f-21-b c-0 py-5" style="padding-top: 10px;">Parakarsa bersama pendamping lintas disiplin ilmu menciptakan produk dokumentasi terbaik.</div>
-                <div class="c-0 pb-5">Kami membuktikan pengalaman terbaik bagi UMKM dan Freelancer berkokreasi membuat dokumentasi sederhana, yang lebih detail & dapat dikembangkan lebih lanjut yang tentunya sebagai bukti naik kelas.</div>
+                <div class="f-21-b c-0 py-5">Parakarsa bersama pendamping lintas disiplin ilmu menciptakan produk dokumentasi terbaik.</div>
+                <div class="c-0 pb-5" style="padding-top: 10px;">Kami membuktikan pengalaman terbaik bagi UMKM dan Freelancer berkokreasi membuat dokumentasi sederhana, yang lebih detail & dapat dikembangkan lebih lanjut yang tentunya sebagai bukti naik kelas.</div>
                 <div class="btn invert stroke-0">Lebih Banyak Dokumen</div>
             </div>
             <div class="col-md-6">
