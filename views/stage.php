12776ce381b3e3cdd19f988574ce52b510addbb4
RafiAhnaf <110021906+RafiAhnaf@users.noreply.github.com>
2022-08-01 14:43:26 +0700

    update

--- views/stage.php
+++ views/stage.php
@@ -297,11 +297,11 @@
 
 
 <!-- Testimonial Start -->
-<div class="container-fluid bg-testimonial py-5 btl-100" style="margin-top: 160px;">
+<div class="container-fluid bg-testimonial py-5 btl-100" style="margin-top: 160px; height: 560px;">
     <div class="container py-5">
         <div class="row">
             <div class="col-lg-7 pt-lg-5 pb-5">
-                <h1 class="section-title mb-5" style=" color: var(--color-theme-2); font-size: 32px;"><b>Bagaimana </b>Kesan Mereka?</h1>
+                <h1 class="section-title mb-5" style=" color: var(--color-theme-2); padding-top: 60px; font-size: 32px;"><b>Bagaimana </b>Kesan Mereka?</h1>
                 <div class="owl-carousel testimonial-carousel position-relative">
                     <div class="d-flex flex-column text-white">
                         <div class="d-flex align-items-center mb-3">
