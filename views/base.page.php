1f36a6a9f9249b21e5a9ce6f4fa94459fc79bf6d
RafiAhnaf <110021906+RafiAhnaf@users.noreply.github.com>
2022-08-01 16:36:57 +0700

    update

--- views/base.page.php
+++ views/base.page.php
@@ -32,8 +32,8 @@
 <body>
     <div class="wrapper bc-2">
         <!-- Top Bar Start -->
-        <div class="top-bar">
-            <div class="container-fluid" style="padding-bottom: 20px; margin-top: 20px;">
+        <div class="top-bar" style="margin: 20px 0px 20px;
+            <div class="container-fluid">
                 <div class="row align-items-center no-gutters">
                     <div class="col-lg-4 col-md-7 d-none d-lg-block">
                         <div class="line-3 c-1 bc-1"></div>
