bd970fc270535b3e0578aebfda297f1f5ad38987
RafiAhnaf <110021906+RafiAhnaf@users.noreply.github.com>
2022-08-01 16:45:48 +0700

    update

--- views/base.page.php
+++ views/base.page.php
@@ -62,7 +62,7 @@
                     <div class="col-lg-4 col-md-12">
                         <div class="logo">
                             <a href="/">
-                                <img src="picture/logo.png" alt="Logo" style="height: auto; width: 200px;">
+                                <img src="picture/logo.png" alt="Logo" style="height: 100px; width: 200px;">
                             </a>
                         </div>
                     </div>
