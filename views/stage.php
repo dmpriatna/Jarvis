9507885ff071e918d50f709c0527ec14804bf312
RafiAhnaf <110021906+RafiAhnaf@users.noreply.github.com>
2022-08-01 15:41:57 +0700

    update

--- views/stage.php
+++ views/stage.php
@@ -409,7 +409,7 @@
 
 <!-- Team Start -->
 <div class="team bc-2">
-    <div class="container mt-10">
+    <div class="container" style="margin-top: 120px;">
         <div class="section-header text-center">
             <h2><b>The Actor </b>Behind Parakarsa</h2>
         </div>
