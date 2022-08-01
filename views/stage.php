37bba2a31f7609be91a229bdd5eac44871708adf
RafiAhnaf <110021906+RafiAhnaf@users.noreply.github.com>
2022-08-01 15:48:39 +0700

    update

--- views/stage.php
+++ views/stage.php
@@ -377,7 +377,7 @@
                 <div class="feature-item bc-3">
                     <div class="feature-text">
                         <div class="d-flex"><img src="picture/people.svg" alt="people" style="margin-right: 10px; width:62px; height:62px;">
-                            <h3 style="padding-top: 12px;"><b>Para</b>Connect</h3>
+                            <h3 style="padding-top: 15px;"><b>Para</b>Connect</h3>
                         </div>
                         <p class="pt-4" style="font-size: 16px;">Program Komunitas UKM & Freelancer yang bertujuan untuk Mengaktivasi dan Meningkatkan Keterlibatan Aktivitas komunitas.</p>
                         <br>
@@ -391,7 +391,7 @@
                 <div class="feature-item">
                     <div class="feature-text">
                         <div class="d-flex"><img src="picture/papers.svg" alt="papers" style="margin-right: 10px; padding-bottom:20px;width:62px; height:62px;">
-                            <h3 style="padding-top: 12px;"><b>Para</b>Beyond</h3>
+                            <h3 style="padding-top: 10px;"><b>Para</b>Beyond</h3>
                         </div>
                         <p class="pt-3" style="font-size: 16px;">Program Eskalasi Enterprise & Job Ecosystems yang bertujuan Melampaui Realisasi Sebelumnya.</p>
                         <br>
