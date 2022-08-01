bb7f3e5c70a820d9f1b0b117d76d109ff26896fc
RafiAhnaf <110021906+RafiAhnaf@users.noreply.github.com>
2022-08-01 15:07:27 +0700

    update

--- views/stage.php
+++ views/stage.php
@@ -308,10 +308,10 @@
                             <img class="img-fluid" src="picture/testimonial-1.jpeg" alt="">
                             <div class="ml-3">
                                 <h5 class="" style=" color: var(--color-theme-3); font-size: 20px; font-weight: medium; font-family: Lora;">Rina Mulyani, S.Pd, M.T</h5>
-                                <i font-size="16px">Kabid. UMKM DISDAGKOPERIN Kota Cimahi</i>
+                                <i style="font-size: 16px;">Kabid. UMKM DISDAGKOPERIN Kota Cimahi</i>
                             </div>
                         </div>
-                        <p>Terima kasih kepada Parakarsa yang telah membantu pemerintah Cimahi dalam meningkatkan
+                        <p class="pt-1">Terima kasih kepada Parakarsa yang telah membantu pemerintah Cimahi dalam meningkatkan
                             kualitas UMKM, khususnya wirausaha pemula di Cimahi melalui
                             pendampingan NATAR.</p>
                     </div>
