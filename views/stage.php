8e8898a62cb8e93bb4d8a24e6dcc49d09c08a78c
RafiAhnaf <110021906+RafiAhnaf@users.noreply.github.com>
2022-08-04 16:37:36 +0700

    update

--- views/stage.php
+++ views/stage.php
@@ -136,8 +136,8 @@
         <div class="row py-5">
             <div class="col-md-6">
                 <h1 class="v-line-6" style="padding-left: 25px;"><b>Hasil Tidak Akan</b> Mengkhianati Usaha</h1>
-                <div class="f-21-b c-0 py-5">Parakarsa bersama pendamping lintas disiplin ilmu menciptakan produk dokumentasi terbaik.</div>
-                <div class="c-0 pb-5">Kami membuktikan pengalaman terbaik bagi UMKM dan Freelancer berkokreasi membuat dokumentasi sederhana, yang lebih detail & dapat dikembangkan lebih lanjut yang tentunya sebagai bukti naik kelas.</div>
+                <div class="f-21-b c-0" style="padding-top: 40px;">Parakarsa bersama pendamping lintas disiplin ilmu menciptakan produk dokumentasi terbaik.</div>
+                <div class="c-0" style=" paddind-top: 20px;">Kami membuktikan pengalaman terbaik bagi UMKM dan Freelancer berkokreasi membuat dokumentasi sederhana, yang lebih detail & dapat dikembangkan lebih lanjut yang tentunya sebagai bukti naik kelas.</div>
                 <div class="btn invert stroke-0" style="margin-top: 40px;">Lebih Banyak Dokumen</div>
             </div>
             <div class="col-md-6">
@@ -357,7 +357,7 @@
 <div class="feature wow fadeInUp" data-wow-delay="0.1s">
     <div class="container-fluid bc-1">
     <div class="c-3 bc-3" style="width: auto; border-style: solid; border-color: inherit;"></div>
-        <h1 class="text-center c-2 f-50 py-5" style="font-size: 40px;"><b>Program Unggulans Untuk</b> UMKM Enthusiasm!</h1>
+        <h1 class="text-center c-2 f-50 py-5" style="font-size: 40px;"><b>Program Unggulan Untuk</b> UMKM Enthusiasm!</h1>
         <div class="line-1 c-3 bc-3"></div>
         <div class="row align-items-center">
             <div class="col-lg-4 col-md-12">
