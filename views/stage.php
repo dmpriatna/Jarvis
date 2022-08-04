5a042319f23b53a0d59b8a4d9c030039892c201f
RafiAhnaf <110021906+RafiAhnaf@users.noreply.github.com>
2022-08-04 16:10:50 +0700

    update

--- views/stage.php
+++ views/stage.php
@@ -358,7 +358,7 @@
     <div class="container-fluid bc-1">
     <div class="c-3 bc-3" style="width: auto; border-style: solid; border-color: inherit;"></div>
         <h1 class="text-center c-2 f-50 py-5" style="font-size: 40px;"><b>Program Unggulan Untuk</b> UMKM Enthusiasm!</h1>
-        <div class="line-1 c-3 bc-3"></div>
+        <div class="line-1 c-3 bc-2"></div>
         <div class="row align-items-center">
             <div class="col-lg-4 col-md-12">
                 <div class="feature-item">
