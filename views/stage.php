30573a57019572af9961c2c4ee6edb184007c5cf
RafiAhnaf <110021906+RafiAhnaf@users.noreply.github.com>
2022-08-04 16:24:08 +0700

    update

--- views/stage.php
+++ views/stage.php
@@ -360,7 +360,7 @@
         <h1 class="text-center c-2 f-50 py-5" style="font-size: 40px;"><b>Program Unggulan Untuk</b> UMKM Enthusiasm!</h1>
         <div class="line-1 c-3 bc-3"></div>
         <div class="row align-items-center">
-            <div class="col-lg-4 col-md-12 hbg">
+            <div class="col-lg-4 col-md-12">
                 <div class="feature-item">
                     <div class="feature-text">
                         <div class="d-flex"><img src="picture/ribbon.svg" alt="ribbon" style="margin-right: 10px; width:62px; height:62px;">
