5dbd3aee2a484f0c18d008931a698d485d15397b
RafiAhnaf <110021906+RafiAhnaf@users.noreply.github.com>
2022-08-08 15:52:59 +0700

    update

--- views/story.php
+++ views/story.php
@@ -91,11 +91,11 @@
         <div class="u-clearfix u-expanded-width u-gutter-30 u-layout-wrap u-layout-wrap-1">
           <div class="u-layout">
             <div class="u-layout-row">
-              <div class="u-container-style u-layout-cell u-left-cell u-size-15 u-size-35-md u-layout-cell-1">
+              <div class="u-container-style u-layout-cell u-left-cell u-size-15 u-size-30-md u-layout-cell-1">
                 <div class="u-container-layout u-valign-middle">
                   <h3 class="u-text u-text-1" style="font-weight: bold; left: 0; color: var(--color-theme-1);">Parakarsa</h3>
-                  <h3 class="u-text u-text-1" style="font-weight: regular; letter-spacing: 20px; font-size: 25px; color: var(--color-theme-1);">GALLERY</h3>
-                  <p>Friday mood at maximum, time for design fun!</p>
+                  <h3 class="u-text u-text-1" style="font-weight: regular; letter-spacing: 15px; font-size: 25px; color: var(--color-theme-1);">GALLERY</h3>
+                  <p class="u-text">Friday mood at maximum, time for design fun!</p>
                 </div>
               </div>
               <div class="u-container-style u-image u-layout-cell u-size-15 u-size-30-md u-image-1">
