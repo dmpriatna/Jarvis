91298a3274815bb5f0a2303ef886608ac3f34e75
RafiAhnaf <110021906+RafiAhnaf@users.noreply.github.com>
2022-08-08 16:23:36 +0700

    update

--- views/story.php
+++ views/story.php
@@ -117,12 +117,12 @@
       <img src="picture/team-1.jpg" alt="" class="u-image u-image-default u-image-1" style="border-top-right-radius: 80px;">
       <div class="u-container-style u-group u-group-1">
         <div class="u-container-layout u-valign-middle-lg u-valign-middle-md u-container-layout-1">
-          <h2 class="v-line-6 u-text u-text-1" style="font-size: 40px; color: var(--color-theme-2);"><b>Customer</b><br> Focus
+          <h2 class="v-line-6 u-text u-text-1" style="font-size: 40px; color: var(--color-theme-3);"><b>Customer</b><br> Focus
           </h2>
-          <h6 style="font-weight: regular; font-family: Lora; color: var(--color-theme-3);">#Terusberdampingan bersama Parakarsa</h6>
+          <h6 style="font-weight: regular; font-family: Lora; color: var(--color-theme-2);">#Terusberdampingan bersama Parakarsa</h6>
           <h6 class="u-text u-text-body-alt-color u-text-2" style="font-weight: medium; font-family: Lora;">We are always on the quest of creating innovative and
             inspirational components of content experience.</h6>
-          <p class="u-text u-text-body-alt-color u-text-3" style="font-weight: italic;">Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+          <p class="u-text u-text-body-alt-color u-text-3">Sed ut perspiciatis unde omnis iste natus error sit voluptatem
             accusantium doloremque <br>laudantium, totam rem sadips ipsums aperiam, eaque ipsa quae ab illo inventore.
             <br>
             <br>
