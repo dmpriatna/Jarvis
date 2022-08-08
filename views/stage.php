24dfaab7ea68b7c7c41834dafa8dff26c843a172
RafiAhnaf <110021906+RafiAhnaf@users.noreply.github.com>
2022-08-08 14:14:24 +0700

    update

--- views/stage.php
+++ views/stage.php
@@ -429,7 +429,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-lg-4 col-md-12 bc-1 one">
+            <div class="col-lg-4 col-md-12 bc-1">
                 <div class="">
                     <div class="feature-text">
                         <div class="d-flex"><img src="picture/papers.svg" alt="papers" style="margin-right: 10px; padding-bottom:20px;width:62px; height:62px;">
