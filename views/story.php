830af6a0f20745dc83b9db2d1666f22f3dae3ee8
RafiAhnaf <110021906+RafiAhnaf@users.noreply.github.com>
2022-08-10 17:43:58 +0700

    update

--- views/story.php
+++ views/story.php
@@ -56,14 +56,15 @@
       <hr style="width: 100%; height: 4px; border: 0 none; background-color: var(--color-theme-1);"></hr>
       <p style="margin-top: 15px; font-size: 60px; margin-left: auto; margin-right: auto; text-align: center; color: var(--color-theme-1); font-family: Modeco Trial;"><b>About</b> Parkarsa</p>
       <hr style="margin-top: 50px; transform: rotate(90deg); width: 10%; height: 2px; border: 0 none; background-color: var(--color-theme-1);"></hr>
-      <p style="text-align: right;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim 
-        ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip 
-        ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate 
-        velit esse cillum dolore eu fugiat nulla pari atur.Lorem ipsum dolor sit amet,
-        consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore 
-        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi 
-        ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate 
+      <p style="text-align: right; font-size: 16px;">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do <br>
+        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim <br>
+        ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip <br>
+        ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate <br>
+        velit esse cillum dolore eu fugiat nulla pari atur.Lorem ipsum dolor sit amet, <br>
+        consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore <br>
+        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi <br>
+        ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate <br>
         velit esse cillum dolore eu fugiat nulla pari atur.</p>
 
     <div style="margin-top: 300px;">
