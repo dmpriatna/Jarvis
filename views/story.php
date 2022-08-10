73a3b80b7f41a9c6ebbbe6771d73f7b5b187f360
RafiAhnaf <110021906+RafiAhnaf@users.noreply.github.com>
2022-08-10 17:39:21 +0700

    update

--- views/story.php
+++ views/story.php
@@ -58,7 +58,7 @@
       <hr style="transform: rotate(90deg); width: 10%; height: 4px; border: 0 none; background-color: var(--color-theme-1);"></hr>
 
 
-    <div style="position: absolute;">
+    <div style="margin-top: 200px;">
       <hr style="width: 100%; height: 4px; border: 0 none; background-color: var(--color-theme-1);"></hr>
       <hr style="width: 100%; height: 4px; border: 0 none; background-color: var(--color-theme-1);"></hr>
     </div>
