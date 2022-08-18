acbd2f599dd3e28ead0737fc7e221448158441a8
RafiAhnaf <110021906+RafiAhnaf@users.noreply.github.com>
2022-08-18 16:46:39 +0700

    update

--- views/actor.detail.php
+++ views/actor.detail.php
@@ -99,24 +99,17 @@
         <p style="font-size: 16px; color: var(--color-theme-0);">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pari atur. Excepteur sint occaecat cupidatat non.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu.</p>
     </div>
 </div>
-<div class="container" style="margin-top: 40px;">
-    <div>
-        <h3 style="text-align: center; font-size: 40px;"><b>MEMBER's</b>GALLERY</h3>
-    </div>
-    <div class="row" style="margin-top: 40px;">
-        <div class="col-md-6">
-            <img src="picture/portfolio-1.jpg" alt="">
-        </div>
-        <div class="col-md-6">
-            <img src="picture/portfolio-2.jpg" alt="">
-        </div>
-    </div>
+<div class="container" style="margin-top: 60px;">
     <div class="row">
-        <div class="col-md-6">
-            <img src="picture/portfolio-3.jpg" alt="">
+        <div class="col-md-4">
+            <img src="picture/portfolio-1.jpg" alt="" style="border-top-right-radius: 80px; height: 400px; width: 100%; object-fit: cover;">
         </div>
-        <div class="col-md-6">
-            <img src="picture/portfolio-4.jpg" alt="">
+        <div>
+            <h1 style="font-family: Modeco; color: var(--color-theme-3);"><b>MEMBER's</b> GALLERY</h1>
+        </div>
+        <div class="col" style="margin-left: 60px; display: grid; align-items: center; align-content: space-evenly;">
+            <h1 style="font-weight: bold; font-size: 40px;"><b>Sedang Mengikuti Event</b> UMKM Kota Bandung</h1>
+            <p style="margin-top: -60px; color: var(--color-theme-0); font-size: 16px;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse.</p>
         </div>
     </div>
 </div>
