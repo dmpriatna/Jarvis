9d4434aa004b518fba217e4a0f7dfba2e0fa549c
RafiAhnaf <110021906+RafiAhnaf@users.noreply.github.com>
2022-08-18 16:24:25 +0700

    update

--- views/actor.detail.php
+++ views/actor.detail.php
@@ -29,7 +29,7 @@
                 <div class="col-5">
                     <img src="picture/team-1.jpg" alt="" style="border-radius: 100%; height: 50px; width: 50px; object-fit: cover;">
                 </div>
-                <div class="col-5" style="font-size: 16px;">
+                <div class="col-5" style="font-size: x-small;">
                     <b>Client Name</b>
                     <span>Profession</span>
                 </div>
@@ -92,9 +92,9 @@
         </div>
     </div>
 </div>
-<div class="container">
+<div class="container" style="margin-top: 60px;">
     <div class="row">
-        <h2>Data Science Project Proposal Presentation</h2>
+        <h2 style="font-weight: bold;">Data Science Project Proposal Presentation</h2>
         <p>Having lots of data means nothing if you don't know how to understand them or how to extract useful knowledge from them. Data science revolves around this, despite being a relatively new field of science. Try to achieve success with a new project on data science. The first step? Presenting your proposal.</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pari atur. Excepteur sint occaecat cupidatat non.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pari atur.</p>
     </div>
