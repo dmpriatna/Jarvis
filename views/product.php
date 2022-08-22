5d86cc95d8c2668a0281be1d33324110513dde75
RafiAhnaf <110021906+RafiAhnaf@users.noreply.github.com>
2022-08-22 14:41:51 +0700

    update

--- views/product.php
+++ views/product.php
@@ -87,7 +87,7 @@
 <div class="container-fluid p-5 section-4">
     <div class="row">
         <div class="col-5">
-            <img src="picture/Garry.svg" alt="">
+            <img src="picture/Garry.svg" alt="" style="border-top-left-radius: 80px;">
         </div>
         <div class="col-7">
             <h2 class="p2"><b>I'm Garry Septiana</b></h2>
@@ -96,7 +96,7 @@
                 <div class="col-1">
                 </div>
                 <div class="col-5">
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard
+                    <p style="color: var(--color-theme-0);">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard
                         dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen
                         book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially
                         unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more
@@ -106,7 +106,7 @@
                     <img style="padding-left:10px" src="picture/vektor-3.svg" alt="">
                 </div>
                 <div class="col-5">
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard
+                    <p style="color: var(--color-theme-0);">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard
                         dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen
                         book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially
                         unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more
