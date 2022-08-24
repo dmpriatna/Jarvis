615cc4071a423cafe288511923d6657722ecaf8c
dmpriatna <dedemaulanapriatna@gmail.com>
2022-08-25 06:22:10 +0700

    Create program.php

---
+++ views/program.php
@@ -0,0 +1,70 @@
+<!-- Content 1 Start -->
+<div>
+<h1>Current in Development</h1>
+</div>
+<!-- Content 1 End -->
+
+<!-- Content 2 Start -->
+<div class="container-fluid p-5 section-2">
+<div class="row">
+    <div class="col-4">
+        <img src="../picture/AboutUs.svg" alt="">
+    </div>
+    <div class="col-8">
+        <h1 class="text-color" style="font-size:70px"><b>About</b> Us</h1>
+        <div class="row" style="padding-top:20px">
+            <div class="col-6">
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard
+                    dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen
+                    book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially
+                    unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more
+                    recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                </p>
+            </div>
+            <div class="col-1">
+                <img style="padding-left:10px" src="../picture/Vector102.svg" alt="">
+            </div>
+            <div class="col-5">
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard
+                    dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen
+                    book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially
+                    unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more
+                    recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Content 2 End -->
+
+<!-- Content 3 Start -->
+<div class="container-fluid p-5 section-3">
+    <div class="row">
+        <div class="col-4">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+                dolor in reprehenderit in Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempsed do eiusmod tempor
+                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veLorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                do eiusmod tempor incididunt ut labx ea commodo consequat. Duis aute irure or incididunt ut labore et dolore magna aliqua. Ut
+                encommodo consequat. Duim ad minim veLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                ut labx ea commodo consequat.</p>
+            <div>
+                <button class="button4"><b>Read More About Us</b></button>
+            </div>
+        </div>
+
+        <div class="col-8">
+            <img src="../picture/paraphoto.svg" alt="">
+        </div>
+    </div>
+</div>
+<!-- Content 3 End -->
+
+<!-- Content 4 Start -->
+<div class="container-fluid p-5 section-4">
+    <h2 class="p2" style="text-align:center;"><b>Online Process-oriented Trainung Groups</b></h2>
+    <p style="text-align:center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+        <br>ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitati
+    </p>
+</div>
+<!-- Content 4 End -->
