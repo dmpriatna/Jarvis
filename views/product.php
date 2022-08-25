45ca48b9ad68d2c18e389a86a0214baa31dd198d
RafiAhnaf <110021906+RafiAhnaf@users.noreply.github.com>
2022-08-25 20:42:37 +0700

    update

--- views/product.php
+++ views/product.php
@@ -135,11 +135,11 @@
                             <i class="bullet" style="font-size: 25px;font-weight: bold; color: var(--color-theme-1);">1</i>
                             <hr style="margin-left: 20px; width: 80%; border: 1px solid var(--color-theme-1);">        
                                </div>
-                                <h4 style="padding-top: 10px; font-weight: bold; font-family: Modeco;">Optimizing your site for mobile devices</h4>
+                                <h4 style="padding-top: -5px; font-weight: bold; font-family: Modeco;">Optimizing your site for mobile devices</h4>
                                 <p>One of the keys of great SEO is having a mobile friendly website which works smoothly on all devices</p>
                             </div>
                         </li>
-                        <div style="margin-top: 50px;">
+                        <div style="margin-top: 30px;">
                         <div class="col-lg-10">    
                         <li class="media">
                             <i class="bullet" style="font-size: 25px;font-weight: bold; color: var(--color-theme-1);">2</i>
@@ -149,13 +149,13 @@
                                 <p>One of the keys of great SEO is having a mobile friendly website which works smoothly on all devices</p>
                             </div>
                         </li>
-                        <div style="margin-top: 10px;">
+                        <div style="margin-top: 30px;">
                         <div class="col-lg-10">    
                         <li class="media">
                             <i class="bullet" style="font-size: 25px;font-weight: bold; color: var(--color-theme-1);">3</i>
                             <hr style="margin-left: 20px; width: 80%; border: 1px solid var(--color-theme-1);">        
                                </div>
-                                <h4 style="padding-top: 10px; font-weight: bold; font-family: Modeco;">Optimizing your site for mobile devices</h4>
+                                <h4 style="padding-top: -5px; font-weight: bold; font-family: Modeco;">Optimizing your site for mobile devices</h4>
                                 <p>One of the keys of great SEO is having a mobile friendly website which works smoothly on all devices</p>
                             </div>
                         </li>
