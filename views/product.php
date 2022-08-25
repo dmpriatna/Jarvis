4257cfbbe904168d6a81d24b646b9d22d31d3701
RafiAhnaf <110021906+RafiAhnaf@users.noreply.github.com>
2022-08-25 22:05:15 +0700

    update

--- views/product.php
+++ views/product.php
@@ -344,7 +344,7 @@
                 <div class="col-lg-12">
                     <div class="text-container">
                         <h2 style="color: #FFFFFF; font-weight: bold;">December 22nd 2020 at Innovation Hub Space</h2>
-                        <p style="color: #FFFFFF;>Our mission is to help people do better SEO. The team is excited to invite you to the next SEO Training Course session which undoubtedly will provide you with the necessary skills to improve your online presence. Fill out the form to register and we'll contact you in less than 24h</p>
+                        <p style="color: #FFFFFF;">Our mission is to help people do better SEO. The team is excited to invite you to the next SEO Training Course session which undoubtedly will provide you with the necessary skills to improve your online presence. Fill out the form to register and we'll contact you in less than 24h</p>
                     </div> <!-- end of text-container -->
                 </div> <!-- end of col -->
             </div> <!-- end of row -->
