e4b25ed5884ea7e22249b564945ac01a867dab39
RafiAhnaf <110021906+RafiAhnaf@users.noreply.github.com>
2022-08-25 20:31:22 +0700

    update

--- views/product.php
+++ views/product.php
@@ -133,7 +133,7 @@
                         <div class="col-lg-6">    
                         <li class="media">
                             <i class="bullet" style="margin-left: 20px; font-size: 25px;font-weight: bold; color: var(--color-theme-1);">1</i>
-                            <hr style="width:65%; border: 1px solid var(--color-theme-1);">        
+                            <hr style="width:80%; border: 1px solid var(--color-theme-1);">        
                                </div>
                                 <h4 style="font-weight: bold; font-family: Modeco;">Optimizing your site for mobile devices</h4>
                                 <p>One of the keys of great SEO is having a mobile friendly website which works smoothly on all devices</p>
