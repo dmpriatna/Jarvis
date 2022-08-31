ed2e7b56ac691e0d325af4c98fce1ed870b409e3
RafiAhnaf <110021906+RafiAhnaf@users.noreply.github.com>
2022-08-31 14:52:35 +0700

    update

--- views/actor.php
+++ views/actor.php
@@ -2,9 +2,9 @@
     <div class="row">
         <img src="picture/carousel-3.jpg" alt="carousel-3" height="350px" width="100%" style="object-fit: cover;">
         <div style="position: absolute; background: #00000080; width: 100%; height: 350px; text-align: center; align-content: center; display: grid;">
-            <p style="font-size: 28px; color: var(--color-theme-2); text-align: center;"><b>Raih Keuntungan Dengan</b> Pendampingan Co-Creation</p>
-            <p style="font-size: 20px; text-align: center; color: var(--color-theme-2);">Pendampingan  Dokumentasi Lintas disiplin ilmu untuk mengembangkan,memperbaiki, & Menumbuhkan Bisnis UMKM.</p>
-            <p style="font-size: 16px; text-align: center; color: var(--color-theme-2);">
+            <p style="font-family: Modeco; font-size: 28px; color: var(--color-theme-2); text-align: center;"><b>Raih Keuntungan Dengan</b><br> Pendampingan Co-Creation</p>
+            <p style="font-family: Lora; font-size: 20px; text-align: center; color: var(--color-theme-2);">Pendampingan  Dokumentasi Lintas disiplin ilmu untuk<br> mengembangkan,memperbaiki, & Menumbuhkan Bisnis UMKM.</p>
+            <p style="font-family: Lora; font-size: 16px; text-align: center; color: var(--color-theme-2);">
             <button class="button1" style="font-weight: 600; font-family: Modeco; color: var(--color-theme-1);"><b>Join Our Ecosystem</b></button>
         </div>
     </div>
