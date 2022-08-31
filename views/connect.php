2308d1ce005c40d164cbe5ff8412e58559d4767b
RafiAhnaf <110021906+RafiAhnaf@users.noreply.github.com>
2022-08-31 16:33:00 +0700

    update

--- views/connect.php
+++ views/connect.php
@@ -1,6 +1,22 @@
 <div class="container-fluid no-gutters">
-    <div class="row">
-        <img src="picture/carousel-3.jpg" alt="carousel-3" height="100%" width="100%" style="object-fit: cover;">
-        <div style="position: absolute; background: #00000080; width: 100%; height: 100%; align-content: center; display: grid; padding-left: 75px;"></div>
+<div class="row bc-1">
+        <div class="col-6" style="opacity:0.8">
+            <img class="parakarsa" style="width: 620px;" src="picture/parakarsa-photo.svg">
+        </div>
+
+        <div class="col-6">
+            <div class="container p-3 py-5">
+                <div style="padding-top: 40px; padding-left: 70px;">
+                    <div class="row">
+                        <h2 class="p v-line-6" style=""><b>Raih Keuntungan Dengan</b><br> Pendampingan Co-Creation</h1>
+                    </div>
+                    <p style="font-family: Lora; padding-top: 14px; color: var(--color-theme-2);">Pendampingan  Dokumentasi Lintas disiplin<br>
+                    ilmu untuk mengembangkan, memperbaiki, &<br>
+                    Menumbuhkan Bisnis UMKM.</p><br>
+                    <p style="font-family: Lora; font-size: 16px; color: var(--color-theme-2);"><b>#TerusBerdampingan</b> bersama Parakarsa</p>
+                    <button class="button1" style="font-weight: 600; font-family: Modeco; color: var(--color-theme-1);"><b>Join Our Ecosystem</b></button>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
