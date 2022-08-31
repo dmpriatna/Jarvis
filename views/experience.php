46ed4f012ea0ff012fb7d7e46c65e24a4868384b
RafiAhnaf <110021906+RafiAhnaf@users.noreply.github.com>
2022-08-31 14:22:12 +0700

    update

--- views/experience.php
+++ views/experience.php
@@ -5,7 +5,7 @@
         </div>
 
         <div class="col-6">
-            <div class="container">
+            <div class="container p-3 py-5">
                 <div style="padding-top: 70px; padding-left: 100px">
                     <div class="row">
                         <h1 class="p v-line-6" style="margin-left: 17px;"><b>Raih Keuntungan Dengan</b><br> Pendampingan Co-Creation</h1>
