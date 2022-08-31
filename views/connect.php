d1ffc8617d4f568b25ccbb63c81919bea3d21f53
RafiAhnaf <110021906+RafiAhnaf@users.noreply.github.com>
2022-08-31 16:35:34 +0700

    update

--- views/connect.php
+++ views/connect.php
@@ -20,3 +20,56 @@
         </div>
     </div>
 </div>
+
+<div class="contact wow fadeInUp bc-2" id="connect">
+    <div class="container">
+        <div class="section-header text-center">
+            <p>Connect with us</p>
+            <h2><b>Ready to</b> Co-creation</h2>
+        </div>
+        <div class="row">
+            <div class="col-md-6">
+                <div class="contact-form">
+                    <div id="success"></div>
+                    <form name="sentMessage" id="contactForm" novalidate="novalidate">
+                        <div class="control-group">
+                            <input type="text" class="form-control" id="name" placeholder="Your Name" required="required" data-validation-required-message="Please enter your name" />
+                            <p class="help-block text-danger"></p>
+                        </div>
+                        <div class="control-group">
+                            <input type="email" class="form-control" id="email" placeholder="Your Email" required="required" data-validation-required-message="Please enter your email" />
+                            <p class="help-block text-danger"></p>
+                        </div>
+                        <div class="control-group">
+                            <input type="text" class="form-control" id="subject" placeholder="Organization or Business Name" required="required" data-validation-required-message="Please enter a subject" />
+                            <p class="help-block text-danger"></p>
+                        </div>
+                        <div class="control-group">
+                            <input type="text" class="form-control" id="subject" placeholder="Website or Social Media" required="required" data-validation-required-message="Please enter a subject" />
+                            <p class="help-block text-danger"></p>
+                        </div>
+                        <div class="control-group">
+                            <select class="form-control" name="" id="">
+                                <option value="">Country</option>
+                                <option value="">Indonesia</option>
+                                <option value="">Malaysia</option>
+                            </select>
+                            <p class="help-block text-danger"></p>
+                        </div>
+                        <div class="control-group">
+                            <textarea class="form-control" id="message" placeholder="What do You need to Co-creation" required="required" data-validation-required-message="Please enter your message"></textarea>
+                            <p class="help-block text-danger"></p>
+                        </div>
+                        <div>
+                            <button class="btn normal" type="submit" id="sendMessageButton">Send
+                                Message</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div class="col-md-6" style="border-bottom-right-radius: 90px;">
+                <img src="picture/contact.png" alt="contact" width="100%" height="100%">
+            </div>
+        </div>
+    </div>
+</div>
