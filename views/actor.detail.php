cb15b544bf1c8f4ccf5cf3c1345a2dd97e7f3d2d
RafiAhnaf <110021906+RafiAhnaf@users.noreply.github.com>
2022-08-31 15:02:49 +0700

    update

--- views/actor.detail.php
+++ views/actor.detail.php
@@ -1,13 +1,27 @@
 <div class="container-fluid no-gutters">
-    <div class="row">
-        <img src="picture/carousel-3.jpg" alt="carousel-3" height="350px" width="100%" style="object-fit: cover;">
-        <div style="position: absolute; background: #00000080; width: 100%; height: 350px; text-align: center; align-content: center; display: grid;">
-            <h5 class="c-2">The Community Review's</h5>
-            <h1 class="c-2">Community Impact</h1>
-            <h1 class="c-2">Review's</h1>
+<div class="row bc-1">
+        <div class="col-6" style="opacity:0.8">
+            <img class="parakarsa" style="width: 620px;" src="picture/parakarsa-photo.svg">
+        </div>
+
+        <div class="col-6">
+            <div class="container p-3 py-5">
+                <div style="padding-top: 40px; padding-left: 70px;">
+                    <div class="row">
+                        <h2 class="p v-line-6" style=""><b>Raih Keuntungan Dengan</b><br> Pendampingan Co-Creation</h1>
+                    </div>
+                    <p style="font-family: Lora; padding-top: 14px; color: var(--color-theme-2);">Pendampingan  Dokumentasi Lintas disiplin<br>
+                    ilmu untuk mengembangkan, memperbaiki, &<br>
+                    Menumbuhkan Bisnis UMKM.</p><br>
+                    <p style="font-family: Lora; font-size: 16px; color: var(--color-theme-2);"><b>#TerusBerdampingan</b> bersama Parakarsa</p>
+                    <button class="button1" style="font-weight: 600; font-family: Modeco; color: var(--color-theme-1);"><b>Join Our Ecosystem</b></button>
+                </div>
+            </div>
         </div>
     </div>
 </div>
+
+
 <div class="container" style="margin-top: 60px;">
     <div class="row">
         <div class="col-md-4">
